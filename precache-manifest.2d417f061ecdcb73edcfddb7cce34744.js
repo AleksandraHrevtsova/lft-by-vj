8e2ba7d741e219ed4cddfc86c7553e7a62e9cef7
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-08-21 17:33:30 +0300

    Updates

---
+++ precache-manifest.2d417f061ecdcb73edcfddb7cce34744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3b9c986c71dd0bc2f551797329333b",
+    "url": "/lft-by-vj/index.html"
+  },
+  {
+    "revision": "5caf0638dddf5743ebc1",
+    "url": "/lft-by-vj/static/css/main.28cee334.chunk.css"
+  },
+  {
+    "revision": "a7bc7f825ee7eb49692d",
+    "url": "/lft-by-vj/static/js/2.827eb549.chunk.js"
+  },
+  {
+    "revision": "5caf0638dddf5743ebc1",
+    "url": "/lft-by-vj/static/js/main.759002b2.chunk.js"
+  },
+  {
+    "revision": "6aa4fe0a82c7d4843026",
+    "url": "/lft-by-vj/static/js/runtime~main.d5a06710.js"
+  }
+]);
