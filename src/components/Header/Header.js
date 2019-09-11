f360183a85c26679c8138ba75dffab52c03fd171
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-09-11 18:39:59 +0300

    fonts install

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 import Icon from '../Icon/Icon';
 
@@ -8,9 +7,9 @@ function Header() {
     <header>
       <div className={classes.header_top}>
         <div>Main/Home</div>
-        <div>Contact us/Email</div>
+        <div className={classes.font1}>Email</div>
         <div>language</div>
-        <div>Currency</div>
+        <div className={classes.font1}>Currency</div>
         <div>
           <Icon icon="Logo" width="96" height="96" />
           {/* <img scr="#" alt="Logo" /> */}
@@ -30,9 +29,45 @@ function Header() {
       </div>
       <div className={classes.header_middle}>
         <div>Searching</div>
+        <h1 className={classes.font1}>Vetkasova по-русски</h1>
         <div>Cart</div>
       </div>
       <div className={classes.header_bottom}>
+        <h1 className={classes.font1}>Vetkasova по-русски</h1>
+        <h1 className={classes.font2}>Vetkasova по-русски</h1>
+        <h1 className={classes.font3}>Vetkasova по-русски</h1>
+        <h1 className={classes.font4}>Vetkasova по-русски</h1>
+        <h1 className={classes.font5}>Vetkasova по-русски</h1>
+        <h1 className={classes.font6}>Vetkasova по-русски</h1>
+        <h1 className={classes.font7}>Vetkasova по-русски</h1>
+        <h1 className={classes.font8}>Vetkasova по-русски</h1>
+        <h1 className={classes.font9}>Vetkasova по-русски</h1>
+        <h1 className={classes.font10}>Vetkasova по-русски</h1>
+        <h1 className={classes.font11}>Vetkasova по-русски</h1>
+        <h1 className={classes.font12}>Vetkasova по-русски</h1>
+        <h1 className={classes.font13}>Vetkasova по-русски</h1>
+        <h1 className={classes.font14}>Vetkasova по-русски</h1>
+        <h1 className={classes.font15}>Vetkasova по-русски</h1>
+
+        <h1 className={classes.font16}>Vetkasova по-русски</h1>
+        <h1 className={classes.font17}>Vetkasova по-русски</h1>
+        <h1 className={classes.font18}>Vetkasova по-русски</h1>
+        <h1 className={classes.font19}>Vetkasova по-русски</h1>
+        <h1 className={classes.font20}>Vetkasova по-русски</h1>
+        <h1 className={classes.font21}>Vetkasova по-русски</h1>
+        <h1 className={classes.font22}>Vetkasova по-русски</h1>
+        <h1 className={classes.font23}>Vetkasova по-русски</h1>
+        <h1 className={classes.font24}>Vetkasova по-русски</h1>
+        <h1 className={classes.font25}>Vetkasova по-русски</h1>
+        <h1 className={classes.font26}>Vetkasova по-русски</h1>
+        <h1 className={classes.font27}>Vetkasova по-русски</h1>
+        <h1 className={classes.font28}>Vetkasova по-русски</h1>
+        <h1 className={classes.font29}>Vetkasova по-русски</h1>
+        <h1 className={classes.font30}>Vetkasova по-русски</h1>
+        <h1 className={classes.font31}>Vetkasova по-русски</h1>
+        <h1 className={classes.font32}>Vetkasova по-русски</h1>
+      </div>
+      {/* <div className={classes.header_bottom}>
         <ul className={classes.nav_list}>
           <li>
             <NavLink href="#" className={classes.nav_link}>
@@ -182,7 +217,7 @@ function Header() {
             </NavLink>
           </li>
         </ul>
-      </div>
+      </div> */}
     </header>
   );
 }
